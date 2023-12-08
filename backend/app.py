ef984037b3be123be84755a7b19f01b784f7729c
Alexis-Papazoglou <alex.papazoglu99@gmail.com>
2023-12-08 23:06:52 +0200

    Add hello_world endpoint

--- backend/app.py
+++ backend/app.py
@@ -31,6 +31,12 @@ def predict():
         return jsonify(result)
     except Exception as e:
         return jsonify({'error': f'Error processing image: {str(e)}'}), 400
+    
+    
+@app.route('/hello', methods=['GET'])
+def hello_world():
+    return 'Hello, World!'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
